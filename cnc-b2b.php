098b3c1f666dd612b6205794821ea2712aa0a20e
Srm071100 <shreyash.makadia@aksharsoftsolutions.com>
2023-01-10 16:18:30 +0530

    Changes For V36

--- cnc-b2b.php
+++ cnc-b2b.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Personalised Gift Supply - Listing Tool
  * Description:       The All-in-one Personalised Gift Supply listing tool, helps in listing products, with customisers and order processing. The easiest way to get Personalised Gifts for sale.
- * Version:           0.0.35
+ * Version:           0.0.36
  * Author:            Akshar Soft Solutions
  * Author URI:        http://aksharsoftsolutions.com/
  * License:           GPL v2 or later
@@ -99,8 +99,8 @@ function cnc_b2b_change_post_media_url($url, $image_id)
 
 function cnc_b2b_support_plugin_frontend_scripts()
 {
-	
-	wp_enqueue_style('pgs_frontend_jqueryui', 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.css?ver=533548279', array(), null, "all");
+
+    wp_enqueue_style('pgs_frontend_jqueryui', 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.css?ver=533548279', array(), null, "all");
     wp_enqueue_style('pgs_frontend_style', plugin_dir_url(__FILE__) . 'assets/css/fronend_style.css', array(), null, "all");
     wp_enqueue_script('pgs_frontend_bpopup_script', plugin_dir_url(__FILE__) . 'assets/js/jquery.bpopup.min.js', array(), null, "all");
     wp_enqueue_script('pgs_jquery_ui', 'https://code.jquery.com/ui/1.13.2/jquery-ui.min.js', array(), null, "all");
--- includes/support_ajax.php
+++ includes/support_ajax.php
@@ -3,16 +3,17 @@
 add_action("wp_ajax_cnc_b2b_sync_product_with_woocommerce", "cnc_b2b_sync_product_with_woocommerce");
 add_action("wp_ajax_nopriv_cnc_b2b_sync_product_with_woocommerce", "cnc_b2b_sync_product_with_woocommerce");
 
-function cnc_b2b_sync_product_with_woocommerce() {
+function cnc_b2b_sync_product_with_woocommerce()
+{
     $product_id = $_POST['product_id'];
-  
-	$post_id = cnc_b2b_create_product_for_wooconnerce($product_id,false);
-	
+
+    $post_id = cnc_b2b_create_product_for_wooconnerce($product_id, false);
+
     $results = array(
         "status" => 200,
         "url" => get_edit_post_link($post_id)
     );
-    
+
     echo json_encode($results);
     die();
 }
@@ -21,100 +22,104 @@ function cnc_b2b_sync_product_with_woocommerce() {
 add_action("wp_ajax_cnc_b2b_sync_order_with_pgs", "cnc_b2b_sync_order_with_pgs");
 add_action("wp_ajax_nopriv_cnc_b2b_sync_order_with_pgs", "cnc_b2b_sync_order_with_pgs");
 
-function cnc_b2b_sync_order_with_pgs(){
-    if(isset($_POST['order_id'])){
+function cnc_b2b_sync_order_with_pgs()
+{
+    if (isset($_POST['order_id'])) {
         cnc_b2b_order_sync_by_id($_POST['order_id']);
     }
     die();
 }
 
-function cnc_b2b_order_sync_by_id($order_id){
-    $order = wc_get_order( $order_id );
-    if ( $order ) {
-        foreach ( $order->get_items() as $item_id => $item ) {
+function cnc_b2b_order_sync_by_id($order_id)
+{
+    $order = wc_get_order($order_id);
+    if ($order) {
+        foreach ($order->get_items() as $item_id => $item) {
             $product_id = $item->get_product_id();
-            if(get_post_meta($product_id,"cnc_b2b_bigcommerce_product",true) == "1"){
-                cnc_b2b_order_item_sync_to_pgs($order,$item_id,$item);
+            if (get_post_meta($product_id, "cnc_b2b_bigcommerce_product", true) == "1") {
+                cnc_b2b_order_item_sync_to_pgs($order, $item_id, $item);
             }
         }
     }
 }
 
-function cnc_b2b_order_item_sync_to_pgs($order,$item_id,$item){
-	
-    $custom_field = get_post_meta( $product_id, '_tmcartepo_data', true);
+function cnc_b2b_order_item_sync_to_pgs($order, $item_id, $item)
+{
+
+    $custom_field = get_post_meta($product_id, '_tmcartepo_data', true);
     $data = array(
         "order_id" => $order->get_id(),
         "item_id" => $item_id,
-        "custom_field" => wc_get_order_item_meta( $item_id, 'custom_field', true ),
-        "product_sku" => get_post_meta( $item->get_product_id(), 'cnc_b2b_bigcommerce_sku', true ),
+        "custom_field" => wc_get_order_item_meta($item_id, 'custom_field', true),
+        "product_sku" => get_post_meta($item->get_product_id(), 'cnc_b2b_bigcommerce_sku', true),
         "product_name" => $item->get_name(),
-        "item_number" =>  get_post_meta( $item->get_product_id(), 'cnc_b2b_bigcommerce_sku', true ),
+        "item_number" =>  get_post_meta($item->get_product_id(), 'cnc_b2b_bigcommerce_sku', true),
         "quantity" => $item->get_quantity(),
-        "shipping_type" => wc_get_order_item_meta( $item_id, 'shipping_type', true ),
-        "next_day" => wc_get_order_item_meta( $item_id, 'next_day', true ),
-        "customer_name" => $order->get_billing_first_name()." ".$order->get_billing_last_name(),
-        "address_line_1" => $order->get_billing_address_1(),
-        "address_line_2" => $order->get_billing_address_2(),
-        "town" => $order->get_billing_city(),
-        "county" => $order->get_billing_state(),
-        "postcode" => $order->get_billing_postcode(),
-        "country" =>$order->get_billing_country(),
-        "reference" => wc_get_order_item_meta( $item_id, 'reference', true ),
+        "shipping_type" => wc_get_order_item_meta($item_id, 'shipping_type', true),
+        "next_day" => wc_get_order_item_meta($item_id, 'next_day', true),
+        "customer_name" => (!empty($order->get_shipping_first_name()) && !empty($order->get_shipping_last_name())) ? $order->get_shipping_first_name() . " " . $order->get_shipping_last_name() : $order->get_billing_first_name() . " " . $order->get_billing_last_name(),
+        "address_line_1" => $order->get_shipping_address_1() ? $order->get_shipping_address_1() : $order->get_billing_address_1(),
+        "address_line_2" => $order->get_shipping_address_2() ? $order->get_shipping_address_2() : $order->get_billing_address_2(),
+        "town" => $order->get_shipping_city() ? $order->get_shipping_city() : $order->get_billing_city(),
+        "county" => $order->get_shipping_state() ? $order->get_shipping_state() : $order->get_billing_state(),
+        "postcode" => $order->get_shipping_postcode() ? $order->get_shipping_postcode() : $order->get_billing_postcode(),
+        "country" => $order->get_shipping_country() ? $order->get_shipping_country() : $order->get_billing_country(),
+        "reference" => wc_get_order_item_meta($item_id, 'reference', true),
         "order_notes" => $order->get_customer_note(),
-        "engrave_fonts" => wc_get_order_item_meta( $item_id, 'Engrave Fonts', true ),
-        "font_color" => wc_get_order_item_meta( $item_id, 'Engrave Font Color', true ),
-        "clipart" => wc_get_order_item_meta( $item_id, 'Engrave Clipart', true ),
-        "font_value_1" => wc_get_order_item_meta( $item_id, 'Engrave Font 1', true ),
-        "font_value_2" => wc_get_order_item_meta( $item_id, 'Engrave Font 2', true ),
-        "font_value_3" => wc_get_order_item_meta( $item_id, 'Engrave Font 3', true ),
-        "font_value_4" => wc_get_order_item_meta( $item_id, 'Engrave Font 4', true ),
-        "font_value_5" => wc_get_order_item_meta( $item_id, 'Engrave Font 5', true ),
-        "font_value_6" => wc_get_order_item_meta( $item_id, 'Engrave Font 6', true ),
-        "font_value_7" => wc_get_order_item_meta( $item_id, 'Engrave Font 7', true ),
-        "font_value_8" => wc_get_order_item_meta( $item_id, 'Engrave Font 8', true ),
-        "font_value_9" => wc_get_order_item_meta( $item_id, 'Engrave Font 9', true ),
-        "font_value_10" => wc_get_order_item_meta( $item_id, 'Engrave Font 10', true ),
-        "uploadNewUrl" => wc_get_order_item_meta( $item_id, 'Uploaded File', true ),
-        "print_url" => wc_get_order_item_meta( $item_id, 'Print Preview', true ),
+        "engrave_fonts" => wc_get_order_item_meta($item_id, 'Engrave Fonts', true),
+        "font_color" => wc_get_order_item_meta($item_id, 'Engrave Font Color', true),
+        "clipart" => wc_get_order_item_meta($item_id, 'Engrave Clipart', true),
+        "font_value_1" => wc_get_order_item_meta($item_id, 'Engrave Font 1', true),
+        "font_value_2" => wc_get_order_item_meta($item_id, 'Engrave Font 2', true),
+        "font_value_3" => wc_get_order_item_meta($item_id, 'Engrave Font 3', true),
+        "font_value_4" => wc_get_order_item_meta($item_id, 'Engrave Font 4', true),
+        "font_value_5" => wc_get_order_item_meta($item_id, 'Engrave Font 5', true),
+        "font_value_6" => wc_get_order_item_meta($item_id, 'Engrave Font 6', true),
+        "font_value_7" => wc_get_order_item_meta($item_id, 'Engrave Font 7', true),
+        "font_value_8" => wc_get_order_item_meta($item_id, 'Engrave Font 8', true),
+        "font_value_9" => wc_get_order_item_meta($item_id, 'Engrave Font 9', true),
+        "font_value_10" => wc_get_order_item_meta($item_id, 'Engrave Font 10', true),
+        "uploadNewUrl" => wc_get_order_item_meta($item_id, 'Uploaded File', true),
+        "print_url" => wc_get_order_item_meta($item_id, 'Print Preview', true),
         "sale_price" => $item->get_total()
     );
-    $url="https://personalisedgiftsupply.com/api/reseller-api/v1/order/create";
+    $url = "https://personalisedgiftsupply.com/api/reseller-api/v1/order/create";
     $args = array(
         'headers' => array(
-          'Content-Type' => 'application/json',
-          'token' => get_option("pgs_products_api_key"),
-          'username' => get_option("pgs_username"),
+            'Content-Type' => 'application/json',
+            'token' => get_option("pgs_products_api_key"),
+            'username' => get_option("pgs_username"),
         ),
         'body' => wp_json_encode($data)
     );
-    $responsedata=wp_remote_post($url,$args);
-    $data=wp_remote_retrieve_body($responsedata);
+    $responsedata = wp_remote_post($url, $args);
+    $data = wp_remote_retrieve_body($responsedata);
     $body = json_decode($data, true);
     // print_r($data);
-    update_post_meta($order->get_id(),"cnc_order_id",$body['data']['cnc_order_id']);
-    if($body['statusCode'] == 200){
-        update_post_meta($order->get_id(),"cnc_b2b_order_created",true);
+    update_post_meta($order->get_id(), "cnc_order_id", $body['data']['cnc_order_id']);
+    if ($body['statusCode'] == 200) {
+        update_post_meta($order->get_id(), "cnc_b2b_order_created", true);
     }
 }
 
-function cnc_b2b_is_image_exist($image){
-	$args = array(
+function cnc_b2b_is_image_exist($image)
+{
+    $args = array(
         'post_type' => 'attachment',
         'posts_per_page' => -1,
         'post_status' => 'inherit',
         'meta_query' => array(
             array(
-	            'key' => 'cnc_b2b_reference_url', 
-	            'value' => $image, 
-	            'compare' => '='
+                'key' => 'cnc_b2b_reference_url',
+                'value' => $image,
+                'compare' => '='
             )
         )
-	);
-	$query =  new WP_Query($args);
-    if($query->post_count > 0){
+    );
+    $query =  new WP_Query($args);
+    if ($query->post_count > 0) {
         return $query->post->ID;
-    }else{
-    	return false;
+    } else {
+        return false;
     }
 }
